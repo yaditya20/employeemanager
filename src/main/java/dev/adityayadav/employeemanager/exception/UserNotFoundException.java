a9c3be8b3e608a1be7e756fafa0d7b74f2180b23
Aditya Yadav <yaditya20@gmail.com>
2021-06-21 18:57:08 +0530

    Initial Commit!

---
+++ src/main/java/dev/adityayadav/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package dev.adityayadav.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/dev/adityayadav/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package dev.adityayadav.employeemanager.repo;
+
+import dev.adityayadav.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
